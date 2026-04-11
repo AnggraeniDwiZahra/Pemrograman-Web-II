586bf1882a13e5b733e3458daaf4e7ecbfdbf448
adzahra <2410817220018@mhs.ulm.ac.id>
2026-04-11 08:38:39 +0800

    Add files via upload

---
+++ Modul 1/PRAK102.php
@@ -0,0 +1,10 @@
+<?php 
+    $Tinggi = 5.4; 
+    $Panjang = 8.9; 
+    $Lebar = 14.7;
+    
+    $Volume = (1/3) * $Panjang * $Lebar * $Tinggi;
+
+    echo 'Volume Bangun Ruang Limas Alas Persegi Panjang <br>';
+    echo '<br> Volume = ' . number_format($Volume, 3) . ' m3';
+?> 
