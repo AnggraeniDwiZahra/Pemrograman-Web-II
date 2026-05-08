dcd806bf6646e406a2c8c70e2bf0b2ac1d2ef942
adzahra <2410817220018@mhs.ulm.ac.id>
2026-05-08 13:52:29 +0800

    Add files via upload

---
+++ Modul 3/PRAK303.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Soal 3</title>
+    </head>
+    <body>
+        <form action='' method='post'>
+            Batas Bawah: <input type='number' name='bawah' required> <br>
+            Batas Atas: <input type='number' name='atas' required> <br>
+            <input type='submit' value='Cetak'>
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $bawah = (int)$_POST['bawah'];
+            $atas = (int)$_POST['atas'];
+            $url_bintang = "star-images.png";
+
+            echo "<div style='margin-top: 20px;'>";
+            echo "<h4>Hasil Deret:</h4>";
+
+            $i = $bawah;
+
+            if ($bawah <= $atas) {
+                do {
+                    if (($i + 7) % 5 == 0) {
+                        echo "<img src='$url_bintang' width='20'; vertical-align: middle;'>";
+                    } else {
+                        echo "<span style='font-size: 20px; margin: 5px;'>$i</span>";
+                    }
+
+                    $i++;
+                } while ($i <= $atas);
+            } 
+            echo "</div>";
+        }
+        ?>
+    </body>
+</html>
