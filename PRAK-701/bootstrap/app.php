09fdb46ddf680356b59d25027f9803ac374d4705
adzahra <2410817220018@mhs.ulm.ac.id>
2026-06-11 19:49:41 +0800

    Update app.php

--- PRAK-701/bootstrap/app.php
+++ PRAK-701/bootstrap/app.php
@@ -11,8 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->redirectGuestsTo(function () {
+            return route('login', ['unauthenticated' => true]);
+        });
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
